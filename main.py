9f65fc925727c18f64af636c167a5066c733e877
Siddharth Porwal <siddharth25porwal@gmail.com>
2021-08-23 02:38:50 +0530

    Added 1,3 channel modes, slider issue fixed

--- main.py
+++ main.py
@@ -15,69 +15,12 @@ def index():
     return render_template('index.html')
 
 
-# @app.route('/filtered', methods=['POST'])
-# def image_upload():
-#     coef_dict = request.form.to_dict()
-#     filter_matrix = []
-#     for key in coef_dict:
-#         filter_matrix.append(coef_dict[key])
-#     filter_matrix = np.array(filter_matrix, dtype=np.int8)
-#     filter_matrix = filter_matrix.reshape((5, 5))
-#     img = request.files.get('image')
-#     data = io.BytesIO()
-#     img.save(data)
-#     encoded_img_data = base64.b64encode(data.getvalue())
-#     up_img = Image.open(img)
-#     up_img = up_img.resize((1200, 1200), Image.ANTIALIAS)
-#     red, green, blue = up_img.split()
-#     grn_arr = np.array(green)
-#     red_arr = np.array(red)
-#     blue_arr = np.array(blue)
-#
-#     blur_g = np.array(
-#         [[min(max(np.sum(filter_matrix * grn_arr[5 * i:5 * i + 5, 5 * j:5 * j + 5]), 0), 255) for j in range(240)]
-#          for i in range(240)], dtype=np.int8)
-#     out_green = Image.fromarray(blur_g, 'L')
-#
-#     blur_r = np.array(
-#         [[min(max(np.sum(filter_matrix * red_arr[5 * i:5 * i + 5, 5 * j:5 * j + 5]), 0), 255) for j in range(240)]
-#          for i in range(240)], dtype=np.int8)
-#     out_red = Image.fromarray(blur_r, 'L')
-#
-#     blur_b = np.array(
-#         [[min(max(np.sum(filter_matrix * blue_arr[5 * i:5 * i + 5, 5 * j:5 * j + 5]), 0), 255) for j in range(240)]
-#          for i in range(240)], dtype=np.int8)
-#     out_blue = Image.fromarray(blur_b, 'L')
-#
-#     output = np.dstack((out_red, out_green, out_blue))
-#
-#     out_img = Image.fromarray(output, 'RGB')
-#     f_data = io.BytesIO()
-#     out_img.save(f_data, 'JPEG')
-#     encoded_f_img_data = base64.b64encode(f_data.getvalue())
-#     return render_template('index.html', img_data=encoded_img_data.decode('utf-8'),
-#                            filter_img_data=encoded_f_img_data.decode('utf-8'))
-
 @app.route('/filtered', methods=['POST'])
 def image_upload():
     coef_dict = request.form.to_dict()
     filter_matrix = []
-    filter_matrix_r = []
-    filter_matrix_g = []
-    filter_matrix_b = []
     for key in coef_dict:
         filter_matrix.append(coef_dict[key])
-
-    filter_matrix_r = filter_matrix[0:9]
-    filter_matrix_r = np.array(filter_matrix_r, dtype=np.int8)
-    filter_matrix_r = filter_matrix_r.reshape((3, 3))
-    filter_matrix_g = filter_matrix[9:18]
-    filter_matrix_g = np.array(filter_matrix_g, dtype=np.int8)
-    filter_matrix_g = filter_matrix_g.reshape((3, 3))
-    filter_matrix_b = filter_matrix[18:]
-    filter_matrix_b = np.array(filter_matrix_b, dtype=np.int8)
-    filter_matrix_b = filter_matrix_b.reshape((3, 3))
-
     img = request.files.get('image')
     data = io.BytesIO()
     img.save(data)
@@ -88,23 +31,55 @@ def image_upload():
     grn_arr = np.array(green)
     red_arr = np.array(red)
     blue_arr = np.array(blue)
+    if coef_dict['inlineRadioOptions'] == 'single':
+        filter_matrix = filter_matrix[1:]
+        filter_matrix = np.array(filter_matrix, dtype=np.int8)
+        filter_matrix = filter_matrix.reshape((5, 5))
 
-    blur_g = np.array(
-        [[min(max(np.sum(filter_matrix_g * grn_arr[3 * i:3 * i + 3, 3 * j:3 * j + 3])/9, 0), 255) for j in range(400)]
-         for i in range(400)], dtype=np.int8)
-    out_green = Image.fromarray(blur_g, 'L')
+        blur_g = np.array(
+            [[min(max(np.sum(filter_matrix * grn_arr[5 * i:5 * i + 5, 5 * j:5 * j + 5]), 0), 255) for j in range(240)]
+             for i in range(240)], dtype=np.int8)
+        out_green = Image.fromarray(blur_g, 'L')
 
-    blur_r = np.array(
-        [[min(max(np.sum(filter_matrix_r * red_arr[3 * i:3 * i + 3, 3 * j:3 * j + 3])/9, 0), 255) for j in range(400)]
-         for i in range(400)], dtype=np.int8)
-    out_red = Image.fromarray(blur_r, 'L')
+        blur_r = np.array(
+            [[min(max(np.sum(filter_matrix * red_arr[5 * i:5 * i + 5, 5 * j:5 * j + 5]), 0), 255) for j in range(240)]
+             for i in range(240)], dtype=np.int8)
+        out_red = Image.fromarray(blur_r, 'L')
 
-    blur_b = np.array(
-        [[min(max(np.sum(filter_matrix_b * blue_arr[3 * i:3 * i + 3, 3 * j:3 * j + 3])/9, 0), 255) for j in range(400)]
-         for i in range(400)], dtype=np.int8)
-    out_blue = Image.fromarray(blur_b, 'L')
+        blur_b = np.array(
+            [[min(max(np.sum(filter_matrix * blue_arr[5 * i:5 * i + 5, 5 * j:5 * j + 5]), 0), 255) for j in range(240)]
+             for i in range(240)], dtype=np.int8)
+        out_blue = Image.fromarray(blur_b, 'L')
+    else:
+        filter_matrix_r = []
+        filter_matrix_g = []
+        filter_matrix_b = []
+        filter_matrix_r = filter_matrix[1:10]
+        filter_matrix_r = np.array(filter_matrix_r, dtype=np.int8)
+        filter_matrix_r = filter_matrix_r.reshape((3, 3))
+        filter_matrix_g = filter_matrix[10:19]
+        filter_matrix_g = np.array(filter_matrix_g, dtype=np.int8)
+        filter_matrix_g = filter_matrix_g.reshape((3, 3))
+        filter_matrix_b = filter_matrix[19:]
+        filter_matrix_b = np.array(filter_matrix_b, dtype=np.int8)
+        filter_matrix_b = filter_matrix_b.reshape((3, 3))
 
+        blur_g = np.array(
+            [[min(max(np.sum(filter_matrix_g * grn_arr[3 * i:3 * i + 3, 3 * j:3 * j + 3])/9, 0), 255) for j in range(400)]
+             for i in range(400)], dtype=np.int8)
+        out_green = Image.fromarray(blur_g, 'L')
+
+        blur_r = np.array(
+            [[min(max(np.sum(filter_matrix_r * red_arr[3 * i:3 * i + 3, 3 * j:3 * j + 3])/9, 0), 255) for j in range(400)]
+             for i in range(400)], dtype=np.int8)
+        out_red = Image.fromarray(blur_r, 'L')
+
+        blur_b = np.array(
+            [[min(max(np.sum(filter_matrix_b * blue_arr[3 * i:3 * i + 3, 3 * j:3 * j + 3])/9, 0), 255) for j in range(400)]
+             for i in range(400)], dtype=np.int8)
+        out_blue = Image.fromarray(blur_b, 'L')
     output = np.dstack((out_red, out_green, out_blue))
+
     out_img = Image.fromarray(output, 'RGB')
     f_data = io.BytesIO()
     out_img.save(f_data, 'JPEG')
--- static/script.js
+++ static/script.js
@@ -1,4 +1,14 @@
 $(function(){
+
+      var startingSlide = 0;
+
+      $(".rgb-channel").hide();
+      $(".rgb-channel input").prop('disabled', true);
+      $(".single-channel input").prop('disabled', false);
+      if($("#result-img").attr('src')){
+        startingSlide = 2;
+      }
+
       var swiper = new Swiper(".mySwiper", {
         effect: "cube",
         grabCursor: true,
@@ -13,23 +23,15 @@ $(function(){
         },
       });
 
-       var swiper = new Swiper(".myCarousel", {
+       var swiper1 = new Swiper(".myCarousel", {
         navigation: {
           nextEl: ".swiper-button-next",
           prevEl: ".swiper-button-prev",
         },
+        initialSlide: startingSlide,
       });
 
 
-    function preview(input){
-        if(input.files && input.files[0]){
-            let reader = new FileReader();
-            reader.onload = function(e){
-                $("#show_img").attr('src', e.target.result);
-            };
-            reader.readAsDataURL(input.files[0]);
-        }
-    }
 
     $("#img_file").change(function(){
         preview(this);
@@ -39,7 +41,32 @@ $(function(){
     $("#filter_form").submit(function(){
         $(".loading-square").show();
         $("#result-div").show();
+        swiper1.slideNext();
     });
 
-
 });
+
+function preview(input){
+    if(input.files && input.files[0]){
+        let reader = new FileReader();
+        reader.onload = function(e){
+            $("#show_img").attr('src', e.target.result);
+        };
+        reader.readAsDataURL(input.files[0]);
+    }
+}
+
+function modeSelect(){
+    if($("#single-channel-radio-btn").is(':checked')){
+        $(".single-channel").show();
+        $(".rgb-channel").hide();
+        $(".rgb-channel input").prop('disabled', true);
+        $(".single-channel input").prop('disabled', false);
+    }
+    else{
+        $(".single-channel").hide();
+        $(".rgb-channel").show();
+        $(".rgb-channel input").prop('disabled', false);
+        $(".single-channel input").prop('disabled', true);
+    }
+}
